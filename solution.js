39fe0b9b109b16363a5e8c444a9205e1e7a7905f
Laura Wu <leemaiwu@gmail.com>
2023-04-24 10:30:29 -0600

    Added alternate one line solution

--- solution.js
+++ solution.js
@@ -11,3 +11,16 @@ console.log(isSquare(4)) // true
 console.log(isSquare(-1)) // false
 console.log(isSquare(25)) // true
 console.log(isSquare(26)) // false
+
+
+// Alternate Solution
+
+const perfectSq = (n) => {
+    return Math.sqrt(n) % 1 === 0
+}
+
+console.log(perfectSq(0)) // true
+console.log(perfectSq(4)) // true
+console.log(perfectSq(-1)) // false
+console.log(perfectSq(25)) // true
+console.log(perfectSq(26)) // false
